badc2d2c63f177f646b09a17e9202431535c9d3e
Callahan <Drew.Callahan@lamresearch.com>
2019-03-01 21:49:18 -0800

    LineSensor test sketch

---
+++ src/linesensorTest/linesensorTest.ino
@@ -0,0 +1,34 @@
+#include <LineSensor.h>
+
+unsigned int leftPin = 23;
+unsigned int rightPin = 22;
+
+
+LineSensor *leftsensor = new LineSensor(leftPin);
+LineSensor *rightsensor = new LineSensor(rightPin);
+
+void setup() {
+  // put your setup code here, to run once:
+
+  Serial.begin(115200);
+
+
+}
+
+int leftstat;
+int rightstat;
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+
+  String leftstat = leftsensor->getStatus() ? "OFF TAPE" : "ON TAPE";
+  String rightstat = rightsensor->getStatus() ? "OFF TAPE" : "ON TAPE";
+
+  Serial.print("Left sensor: ");
+  Serial.println(leftstat);
+  Serial.print("Right sensor: ");
+  Serial.println(rightstat);
+  
+  delay(500);
+}
